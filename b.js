3edba652177c27c541a455c5348299579fae2f9c
Heber Barra <heberferreirabarra@gmail.com>
2023-04-24 21:17:04 -0300

    Code refactor: melhorei os nomes da variáveis e também mudei o funcionamento do botão de adicionar clickers para evitar possível bug

--- b.js
+++ b.js
@@ -1,49 +1,62 @@
-let ab = document.getElementById("par");
-let bot = document.getElementById("bot");
-let up = document.getElementById("up");
-let lvl = document.getElementById("level");
+const goldDisplay = document.getElementById("par");
+const gainGoldButton = document.getElementById("bot");
+const upgradeButton = document.getElementById("up");
+const levelDisplay = document.getElementById("level");
 const body = document.getElementsByTagName("body")[0];
-let a = 0;
-let upgs = 1;
-let preco1 = 50;
+let quantityOfClickers = 0;
+let gold = 0;
+let gain = 1;
+let preco = 50;
 let level = 0;
-let at = 0;
+let clickerUnlocked = false;
 
-bot.addEventListener("click", () => {
-	ab.innerText = a + upgs + " g";
-	a += upgs;
+gainGoldButton.addEventListener("click", () => {
+	gold += gain;
+	goldDisplay.innerText = `${gold} g`;
 })
-up.addEventListener("click", () => {
-	if (a >= preco1){
-		upgs++;
-		a -= preco1;
-		ab.innerText = a + " g";
-		preco1 += 50;
-		up.innerText = preco1 + " g";
-		level++;
-		lvl.innerText = "level " + level;
-		if (level == 5 && at == 0){
-			let auto = document.createElement('button');
-			auto.textContent = "Auto criquer: 1000 g";
-			auto.id = "criquer";
-			body.appendChild(auto);
-		}
-	}
-	else {
+
+upgradeButton.addEventListener("click", () => {
+	if (gold < preco) {
 		window.alert("POBRE!");
 	}
+
+	gain++;
+	gold -= preco;
+	goldDisplay.innerText = `${gold} g`;
+	preco += 50;
+	upgradeButton.innerText = `${preco} g`;
+	level++;
+	levelDisplay.innerText = `level ${level}`;
+
+	if (level == 5 && !clickerUnlocked){
+		if (document.getElementById("criquer")) {
+			return;
+		}
+
+		let auto = document.createElement('button');
+		auto.textContent = "Auto criquer: 1000 g";
+		auto.id = "criquer";
+		body.appendChild(auto);
+	}
+
 	let auto = document.getElementById("criquer");
 	if (auto) {
-	auto.addEventListener("click", () => {
-		if (a >= 1000){
-			a -= 1000
-			setTimeout(() => {
-				ab.innerText = ++a;
-			}, 2000);
-		}
-		else {
-			window.alert("POBRE!");
-		}
-	})
+		auto.addEventListener("click", () => {
+			if (gold >= 1000){
+				gold -= 1000;
+				quantityOfClickers++;
+				goldDisplay.innerText = `${gold} g`;
+			}
+			else {
+				window.alert("POBRE!");
+			}
+		})
+
+		clickerUnlocked = true;
 	}
 })
+
+const incrementPoints = () => {
+	gold += quantityOfClickers;
+	goldDisplay.innerText = `${gold} g`;
+}
